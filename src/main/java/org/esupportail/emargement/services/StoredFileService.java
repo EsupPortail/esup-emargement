3710f47c1e5523b9a40c21fa6f6ddb08666517da
Jean-Pierre TRAN <Jean.Pierre-Tran@univ-rouen.fr>
2020-05-25 11:59:56 +0200

    Correction enregistrement contexte PJ

--- src/main/java/org/esupportail/emargement/services/StoredFileService.java
+++ src/main/java/org/esupportail/emargement/services/StoredFileService.java
@@ -4,12 +4,18 @@ import java.io.IOException;
 import java.util.Date;
 
 import org.esupportail.emargement.domain.BigFile;
+import org.esupportail.emargement.domain.Context;
 import org.esupportail.emargement.domain.StoredFile;
+import org.esupportail.emargement.repositories.ContextRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class StoredFileService {
+	
+	@Autowired
+	ContextRepository contextRepository;
 	
 	public StoredFile setStoredFile(StoredFile storedFile, MultipartFile file, String emargementContext) throws IOException {
 		
@@ -17,10 +23,12 @@ public class StoredFileService {
 		storedFile.setFile(file);
 		storedFile.setBigFile(new BigFile());
 		storedFile.getBigFile().setBinaryFile(file.getBytes());
+		Context context = contextRepository.findByKey(emargementContext);
+		storedFile.getBigFile().setContext(context);
 		storedFile.setContentType(file.getContentType());
 		storedFile.setFilename(file.getName());
 		storedFile.setFileSize(file.getSize());
-		
+		storedFile.setContext(context);
 		return storedFile;
 	}
 }
