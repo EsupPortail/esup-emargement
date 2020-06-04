17e8a3431b8d6fb7ab155d18e6d279b7e2611dad
Jean-Pierre TRAN <Jean.Pierre-Tran@univ-rouen.fr>
2020-06-04 13:23:09 +0200

    nettoyage ...

--- src/main/java/org/esupportail/emargement/services/SessionEpreuveService.java
+++ src/main/java/org/esupportail/emargement/services/SessionEpreuveService.java
@@ -43,7 +43,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
--- src/main/java/org/esupportail/emargement/web/manager/TagCheckController.java
+++ src/main/java/org/esupportail/emargement/web/manager/TagCheckController.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import org.esupportail.emargement.domain.ApogeeBean;
 import org.esupportail.emargement.domain.Groupe;
 import org.esupportail.emargement.domain.Person;
 import org.esupportail.emargement.domain.SessionEpreuve;
