7c76159a56c9e08b94bf030d1b0328f36464b025
Jean-Pierre TRAN <Jean.Pierre-Tran@univ-rouen.fr>
2021-05-27 16:39:29 +0200

    Modifications ui

--- src/main/java/org/esupportail/emargement/services/TagCheckService.java
+++ src/main/java/org/esupportail/emargement/services/TagCheckService.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.annotation.Resource;
