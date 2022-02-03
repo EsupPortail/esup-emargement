cd54ee651087df4e0c7d38e807f9b3445595e41a
Vincent Bonamy <vbonamy@gmail.com>
2022-02-03 14:23:53 +0100

    Ldap Config : nettoyage - ldap.userSearchFilter plus utilisé

--- src/main/java/org/esupportail/emargement/config/LdapConfig.java
+++ src/main/java/org/esupportail/emargement/config/LdapConfig.java
@@ -3,25 +3,17 @@ package org.esupportail.emargement.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.search.FilterBasedLdapUserSearch;
 import org.springframework.security.ldap.search.LdapUserSearch;
 import org.springframework.stereotype.Component;
 
-@Component
+
+@Configuration
 @EnableLdapRepositories
 public class LdapConfig {
 
-	private String userSearchFilter;
-
-	@Value("${ldap.userSearchFilter}")
-	public void setUserSearchFilter(String userSearchFilter) {
-		this.userSearchFilter = userSearchFilter;
-	}
-
-	public String getUserSearchFilter() {
-		return userSearchFilter;
-	}
 }
 
