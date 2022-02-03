d5d1335f88a5ef3194d825ab732f3a2569fafa81
Vincent Bonamy <vbonamy@gmail.com>
2022-02-03 14:27:30 +0100

    Ldap Config : nettoyage - ldap.people plus utilisé

--- src/main/java/org/esupportail/emargement/services/LdapService.java
+++ src/main/java/org/esupportail/emargement/services/LdapService.java
@@ -44,9 +44,6 @@ public class LdapService {
 	@Value("${ldap.groups}")
 	private String ldapGroups;
 	
-	@Value("${ldap.people}")
-	private String ldapPeople;
-	
 	@Value("${emargement.ruleSuperAdmin.memberOf}")
 	private String ruleSuperAdmin;
 	
