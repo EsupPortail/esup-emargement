78ffadb46c3c46555904909f18b3ff9cc7d5199c
JPTRAN <jean-pierre.tran@univ-rouen.fr>
2022-05-25 09:57:21 +0200

    Nettoyage.

--- src/main/java/org/esupportail/emargement/web/WebUtils.java
+++ src/main/java/org/esupportail/emargement/web/WebUtils.java
@@ -8,8 +8,6 @@ import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.collections4.ListUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.esupportail.emargement.security.ContextUserDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
