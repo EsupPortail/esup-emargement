8b147a87efd7e3a02058c85e471cd85008f46f4c
Jean-Pierre TRAN <Jean.Pierre-Tran@univ-rouen.fr>
2024-03-15 17:03:44 +0100

    Cache control sur ressources
    Attention : changement dans le fichier emargement properties

--- src/main/java/org/esupportail/emargement/config/WebConfig.java
+++ src/main/java/org/esupportail/emargement/config/WebConfig.java
@@ -8,21 +8,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @Configuration  
 public class WebConfig implements WebMvcConfigurer {  
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    	registry.addResourceHandler("/resources/**")
-        .addResourceLocations("classpath:/static/");
-    	registry.addResourceHandler("/webjars/**")
-            .addResourceLocations("/webjars/").resourceChain(false);
-    }
-    
     @Bean
     public WebInterceptor webInterceptor() {
         return new WebInterceptor();
